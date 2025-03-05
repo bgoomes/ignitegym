bdbc7d245b04f44f38bbdb7426060f0165aa47a7
Bruno Gomes <obrunogomes@obrunogomes.local>
2025-03-05 11:19:41 -0300

    add font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 
 import { Text, View } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fonstLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home</Text>
+      {fonstLoaded ? <Text>Home</Text> : <View />}
     </View>
   );
 }
