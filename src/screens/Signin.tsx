14609d7c65aa4cbe9f52d41feb85aca589a277fa
Bruno Gomes <obrunogomes@obrunogomes.local>
2025-03-08 20:34:36 -0300

    fix: compatibilidade svg

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,5 +1,6 @@
-import { VStack, Image } from "@gluestack-ui/themed"
+import { VStack, Image, Center } from "@gluestack-ui/themed"
 import BackgroundImg from "@assets/background.png"
+import Logo from "@assets/logo.svg"
 
 export function SignIn(){
     return(
@@ -12,6 +13,9 @@ export function SignIn(){
                h={624}
                position="absolute"
             />
+            <Center my="$24">
+                <Logo />
+            </Center>
         </VStack>
     )
 }
